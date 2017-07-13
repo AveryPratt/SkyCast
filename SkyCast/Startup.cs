f9d6b9ecea07e0d6c0969de598fdbcefe856f30a
AveryPratt <apratt91@gmail.com>
2017-07-13 00:07:01 -0700

    Add project files.

---
+++ SkyCast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyCast.Startup))]
+namespace SkyCast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
