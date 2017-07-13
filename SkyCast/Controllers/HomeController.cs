b050e5bbec4c70e576657a6869ac066e890b3dc7
AveryPratt <apratt91@gmail.com>
2017-07-13 00:40:53 -0700

    started setting up controller for configuring api calls

--- SkyCast/Controllers/HomeController.cs
+++ SkyCast/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +9,17 @@ namespace SkyCast.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult CallAPI()
+        {
+            var lat = 0;
+            var lon = 0;
+            string key = "319cbb1ff967b87cc559b2e8308d0ddc";
+            string uri = String.Format("https://api.darksky.net/forecast/{0}/{1},{2}", key, lat, lon);
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(uri).Result;
+            return this.View();
+        }
+
         public ActionResult Index()
         {
             return View();
