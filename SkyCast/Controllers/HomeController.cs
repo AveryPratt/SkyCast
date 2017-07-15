c59822ed4767ba7dcae65232785decc4f36cec90
Avery Pratt <avery.pratt@allyis.com>
2017-07-15 15:51:36 -0700

    both api calls are working, except json is too long

--- SkyCast/Controllers/HomeController.cs
+++ SkyCast/Controllers/HomeController.cs
@@ -1,28 +1,98 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SkyCast.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace SkyCast.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult CallAPI()
+        public ActionResult Geolocation(string location)
         {
-            var lat = 0;
-            var lon = 0;
-            string key = "319cbb1ff967b87cc559b2e8308d0ddc";
-            string uri = String.Format("https://api.darksky.net/forecast/{0}/{1},{2}", key, lat, lon);
-            HttpClient client = new HttpClient();
-            var response = client.GetAsync(uri).Result;
-            return this.View();
+            string geoKey = "AIzaSyAkI1hZCvx9yR-e3YcFAGb9AiaVovAfPpA";
+            string geoUri = String.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}", location, geoKey);
+
+            // call Geo API
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(geoUri).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string geoJson = response.Content.ReadAsStringAsync().Result;
+                        GeoReport geoReport = JsonConvert.DeserializeObject<GeoReport>(geoJson);
+                        Location coordinates = geoReport.results.First().geometry.location;
+                        GeoResult geoResult = new GeoResult()
+                        {
+                            name = location,
+							report = geoReport
+                        };
+						string jsonObj = new JavaScriptSerializer().Serialize(geoResult);
+                        return this.RedirectToAction("WeatherForecast", Json(jsonObj));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.RedirectToAction("Index", "Could not find location.");
+            }
+            return null;
         }
 
-        public ActionResult Index()
+        public ActionResult WeatherForecast(JsonResult jsonResult)
         {
-            return View();
+            string weatherKey = "319cbb1ff967b87cc559b2e8308d0ddc";
+            string weatherUri;
+            GeoResult geoResult;
+
+            // get coordinates for location
+            try
+            {
+				string jsonStr = ((string[])jsonResult.Data).First().ToString();
+				geoResult = new JavaScriptSerializer().Deserialize<GeoResult>(jsonStr);
+				Location coordinates = geoResult.report.results.First().geometry.location;
+                weatherUri = String.Format("https://api.darksky.net/forecast/{0}/{1},{2}", weatherKey, coordinates.lat, coordinates.lng);
+            }
+            catch (Exception ex)
+            {
+                return this.RedirectToAction("Index", "Could not find coordinates for location.");
+            }
+
+            // call Weather API
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(weatherUri).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string weatherJson = response.Content.ReadAsStringAsync().Result;
+                        WeatherResult weatherResult = new WeatherResult()
+                        {
+                            report = JsonConvert.DeserializeObject<WeatherReport>(weatherJson),
+							geoResult = geoResult
+                        };
+						string jsonObject = new JavaScriptSerializer().Serialize(weatherResult);
+						return this.RedirectToAction("Index", Json(jsonObject));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.RedirectToAction("Index", "Could not find weather data.");
+            }
+            return null;
+        }
+
+        public ActionResult Index(WeatherResult model)
+        {
+            return View(model);
         }
 
         public ActionResult About()
---
+++ SkyCast/Models/WeatherReport.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkyCast.Models
+{
+    public class Currently
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public double precipIntensity { get; set; }
+        public double precipProbability { get; set; }
+        public double temperature { get; set; }
+        public double apparentTemperature { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public double pressure { get; set; }
+        public double ozone { get; set; }
+        public int uvIndex { get; set; }
+    }
+
+    public class Datum
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public double precipIntensity { get; set; }
+        public double precipProbability { get; set; }
+        public double temperature { get; set; }
+        public double apparentTemperature { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public double pressure { get; set; }
+        public double ozone { get; set; }
+        public int uvIndex { get; set; }
+    }
+
+    public class Hourly
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public List<Datum> data { get; set; }
+    }
+
+    public class Datum2
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public int sunriseTime { get; set; }
+        public int sunsetTime { get; set; }
+        public double moonPhase { get; set; }
+        public double precipIntensity { get; set; }
+        public double precipIntensityMax { get; set; }
+        public int precipIntensityMaxTime { get; set; }
+        public double precipProbability { get; set; }
+        public string precipType { get; set; }
+        public double temperatureMin { get; set; }
+        public int temperatureMinTime { get; set; }
+        public double temperatureMax { get; set; }
+        public int temperatureMaxTime { get; set; }
+        public double apparentTemperatureMin { get; set; }
+        public int apparentTemperatureMinTime { get; set; }
+        public double apparentTemperatureMax { get; set; }
+        public int apparentTemperatureMaxTime { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windGustTime { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public double pressure { get; set; }
+        public double ozone { get; set; }
+        public int uvIndex { get; set; }
+        public int uvIndexTime { get; set; }
+    }
+
+    public class Daily
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public List<Datum2> data { get; set; }
+    }
+
+    public class Flags
+    {
+        public List<string> sources { get; set; }
+        public string units { get; set; }
+    }
+
+    public class WeatherReport
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string timezone { get; set; }
+        public double offset { get; set; }
+        public Currently currently { get; set; }
+        public Hourly hourly { get; set; }
+        public Daily daily { get; set; }
+        public Flags flags { get; set; }
+    }
+    
+    public class WeatherResult
+    {
+        public WeatherReport report { get; set; }
+        public GeoResult geoResult { get; set; }
+    }
+}
